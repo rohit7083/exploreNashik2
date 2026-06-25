b77d3951ec2c2f2b8cc91765b944e316e533752f
MyTution <rohitsonawane889@gmail.com>
2026-06-25 23:47:33 +0530

    comment out API_KEY logging in server start

--- exploreNashik/backend/index.js
+++ exploreNashik/backend/index.js
@@ -35,5 +35,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  console.log(process.env.API_KEY);
+  // console.log(process.env.API_KEY);
 });
