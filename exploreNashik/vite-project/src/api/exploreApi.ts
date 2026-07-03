ac0b93d5a442b5a48b8f9e15891a75cd295d0405
MyTution <rohitsonawane889@gmail.com>
2026-07-03 06:58:40 +0530

    Place no useds

--- exploreNashik/vite-project/src/api/exploreApi.ts
+++ exploreNashik/vite-project/src/api/exploreApi.ts
@@ -1,20 +1,20 @@
 // exploreApi.ts
 import axios from "axios";
-type Place = {
-  id: number;
-  name: string;
-  description: string;
-  rating: number;
-  reviews: number;
-  category: string;
-  subcategory: string;
-  tags: string[];
-  image?: string;
-  location?: string;
-  distance?: number;
-  bestTime?: string;
-  timings?: string;
-};
+// type Place = {
+//   id: number;
+//   name: string;
+//   description: string;
+//   rating: number;
+//   reviews: number;
+//   category: string;
+//   subcategory: string;
+//   tags: string[];
+//   image?: string;
+//   location?: string;
+//   distance?: number;
+//   bestTime?: string;
+//   timings?: string;
+// };
 
 // export const getPlaces = async (
 //   category?: string,
